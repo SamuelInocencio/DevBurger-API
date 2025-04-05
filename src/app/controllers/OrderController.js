ca9f46f792d9201ecc28dd3e3bd0ad97e7fb1c80
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-04-05 09:23:46 -0300

    segundo commit aula 13

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,16 +1,19 @@
 import * as Yup from 'yup';
 
 import Order from '../schemas/Order';
+import Product from '../models/Product';
 
 class OrderController {
   async store(request, response) {
     const schema = Yup.object({
-      products: Yup.array().required().of(
-        Yup.object({
-          id: Yup.number().required(),
-          quantity: Yup.number().required(),
-        })
-      ),
+      products: Yup.array()
+        .required()
+        .of(
+          Yup.object({
+            id: Yup.number().required(),
+            quantity: Yup.number().required(),
+          }),
+        ),
     });
 
     try {
@@ -19,13 +22,22 @@ class OrderController {
       return response.status(400).json({ error: err.errors });
     }
 
-    // const { name } = request.body;
+    const { products } = request.body;
+
+    const productsIds = products.map((product) => product.id);
+
+    const findProducts = await Product.findAll({
+      where: {
+        id: productsIds,
+      },
+    });
 
     const order = {
       user: {
         id: request.userId,
         name: request.userName,
       },
+      products: findProducts,
     };
 
     return response.status(201).json(order);
