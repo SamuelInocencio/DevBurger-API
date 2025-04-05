df52a351f509bc880f2478a5285730834785b061
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-04-05 08:12:29 -0300

    connected with MongoDB

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import configDatabase from '../config/database';
 
@@ -11,6 +12,7 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(configDatabase);
@@ -19,6 +21,10 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models),
       );
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/devburger');
   }
 }
 
